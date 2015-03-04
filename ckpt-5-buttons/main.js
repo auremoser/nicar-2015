267151f5c5a8a475b8174fdda00fabd2a6de1458
Aurelia Moser <auremoser@gmail.com>
2015-03-04 14:27:29 -0500

    update ckpt 3 and add buttons

--- ckpt-5-buttons/main.js
+++ ckpt-5-buttons/main.js
@@ -3,9 +3,11 @@ window.onload = function () {
    var layerSource = 'http://documentation.cartodb.com/api/v2/viz/ed78c85e-c11b-11e4-ab66-0e853d047bba/viz.json'; // add your url ID here between viz/ and /viz.json
 
    var options = {
-       sql: "SELECT * FROM atl_census_demo_2010",
-       cartocss: "#atl_census_demo_2010{polygon-fill:#0fa59f;}"
+       sql: $("#sql").text(), // here you can set sql to run on your tables
+       cartocss: $("#simple").text() // here you can set css to style your vis
    }
+   // clear sql of space and returns
+   options.sql = options.sql.replace(/(\r\n|\n|\r)/gm,'').trim();
 
    var sublayers = [];
 
@@ -14,6 +16,23 @@ window.onload = function () {
        center: [31.7550, -84.3900], // Atlanta, Georgia
        zoom: 7 // zoom projection to adjust starting point zoom
    });
+
+   // Create layer selector
+   function createSelector(layer) {
+      var cartocss = "";
+      var $options = $(".layer_selector").find("li");
+      $options.click(function(e) {
+          var $li = $(e.target);
+          var selected = $li.attr('data');
+
+          $options.removeClass('selected');
+          $li.addClass('selected');
+
+          cartocss = $('#'+selected).text();
+
+          layer[0].setCartoCSS(cartocss);
+      });
+   }
 
    // add basemap tiles
    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
@@ -29,6 +48,7 @@ window.onload = function () {
            sublayers[1] = layer.getSubLayer(1);
            sublayers[0].set(options); // altering the SQL and CartoCSS; see above
            sublayers[1].hide(); // hiding the traffic data
+           createSelector(sublayers);
        })
        .error(function(err) {
            console.log("Map not created: " + err);
